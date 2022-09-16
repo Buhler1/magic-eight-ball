b9d6a7c3ac5c66c26f5ccc04a7cdf0f303d0e609
Matt Buhler <mattbuhler112@gmail.com>
2022-09-16 14:39:57 -0700

    put finishing touches on the bottom area

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// all posible answers 8 ball can give
 const points = [
   "It is certain",
   "It is decidedly so",
@@ -8,7 +9,7 @@ const points = [
   "Outlook not so good",
   "Signs point to yes"
 ];
-
+// randomly selects an answer for the users question
 let ballAnswer 
 function myFunction(){
   points.sort(function(a,b){return 0.5 - Math.random()});
@@ -16,39 +17,34 @@ function myFunction(){
   ballAnswer = points[0];
   document.getElementById("ballAnswer").style.fontSize="18px";
   setTimeout(timeup, 4000);
-
+// clears text area after 4 seconds and puts answers below
   function timeup(){
-    //document.getElementById("ballAnswer").innerHTML="8";
-    //document.getElementById("ballAnswer").style.fontSize ="125px";
-    //inputAnswer()
     createDiv()
     createP()
     document.getElementById("typeArea").value="";
   }
 }
-
+//puts users question below the 8 ball
 function inputAnswer() {
   const previousQuestion = document.getElementById("typeArea").value;
   userQuestion.innerHTML = previousQuestion
   const questionAsked = document.getElementById("questions-asked")
   questionAsked.appendChild(userQuestion)
 }
-
+// creates div for users questions to go into
 function createDiv() {
   const div = document.createElement("div")
   div.classList.add("question-answers")
   document.getElementById("questions-asked").appendChild(div)
 }
-
+// inputs text of users questions 
 function createP() {
   let userQuestion = document.createElement("p")
   const ballResponse = document.createElement("p")
   userQuestion.innerHTML=document.getElementById("typeArea").value;
   let lastIndex = document.getElementsByClassName("question-answers").length-1;
   const lastElement = document.getElementsByClassName("question-answers")[lastIndex];
-  userQuestion.classList.add("")
   lastElement.appendChild(userQuestion)
   ballResponse.innerHTML=ballAnswer;
-  ballResponse.classList.add("")
   lastElement.appendChild(ballResponse)
 }
